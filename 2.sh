577d197e6c979125ed16e37bd48bc314e36f7739
Jayadev Naram <31660846+JayD1@users.noreply.github.com>
2017-10-20 00:28:48 +0530

    done!! but check again

--- 2.sh
+++ 2.sh
@@ -5,9 +5,9 @@ set -o history
 c=0 
 search=`history | tr -s " " | cut -d" " -f 3 | sort | head -1`
 echo $search `history | grep "$search" | wc -l`
-history | tr -s " " | cut -d" " -f 3 | sort |  while read d; do
-    count=`history | tr -s " " | cut -d" " -f 3 | sort | grep $d | wc -l`
-    search1=$d      
+history | tr -s " " | cut -d" " -f 3 | sort |  while read k; do
+    count=`history | tr -s " " | cut -d" " -f 3 | sort | grep $k | wc -l`
+    search1=$k      
     if [ $search1 != $search ];then
         echo $search1 $count
         search=$search1 
